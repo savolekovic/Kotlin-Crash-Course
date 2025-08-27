d2484922a10aa4abf602410a667ed33a73f50855
Savo Lekovic <savo.lekovic@amplitudo.me>
2025-08-27 14:02:39 +0200

    Initial commit

---
+++ src/main/kotlin/me/vosaa/kotlin_course/KotlinCourseApplication.kt
@@ -0,0 +1,11 @@
+package me.vosaa.kotlin_course
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCourseApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinCourseApplication>(*args)
+}
---
+++ src/test/kotlin/me/vosaa/kotlin_course/KotlinCourseApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.vosaa.kotlin_course
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCourseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
