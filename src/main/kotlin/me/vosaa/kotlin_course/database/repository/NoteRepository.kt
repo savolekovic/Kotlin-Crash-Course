8ae9418fb05bb3a278097106dc3d86af23a1637c
Savo Lekovic <savo.lekovic@amplitudo.me>
2025-08-28 14:03:06 +0200

    creates note repo

---
+++ src/main/kotlin/me/vosaa/kotlin_course/database/repository/NoteRepository.kt
@@ -0,0 +1,9 @@
+package me.vosaa.kotlin_course.database.repository
+
+import me.vosaa.kotlin_course.database.model.Note
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface NoteRepository : MongoRepository<Note, ObjectId> {
+    fun findByOwnerId(ownerId: ObjectId): List<Note>
+}
