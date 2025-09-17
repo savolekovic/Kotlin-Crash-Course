f4fdecdd4b3ee8b49b1310aef52f934e73b3bf13
Savo Lekovic <savo.lekovic@amplitudo.me>
2025-09-17 13:49:05 +0200

    creates AuthService

---
+++ src/main/kotlin/me/vosaa/kotlin_course/security/AuthService.kt
@@ -0,0 +1,113 @@
+package me.vosaa.kotlin_course.security
+
+import me.vosaa.kotlin_course.database.model.RefreshToken
+import me.vosaa.kotlin_course.database.model.User
+import me.vosaa.kotlin_course.database.repository.RefreshTokenRepository
+import me.vosaa.kotlin_course.database.repository.UserRepository
+import org.bson.types.ObjectId
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.stereotype.Service
+import java.security.MessageDigest
+import java.time.Instant
+import java.util.*
+
+@Service
+class AuthService(
+    private val jwtService: JwtService,
+    private val userRepository: UserRepository,
+    private val hashEncoder: HashEncoder,
+    private val refreshTokenRepository: RefreshTokenRepository
+) {
+
+    data class TokenPair(
+        val accessToken: String,
+        val refreshToken: String,
+    )
+
+    fun register(email: String, password: String): User {
+        return userRepository.save(
+            User(
+                email = email,
+                hashedPassword = hashEncoder.encode(password)
+            )
+        )
+    }
+
+    fun login(email: String, password: String): TokenPair {
+        val user = userRepository.findByEmail(email)
+            ?: throw BadCredentialsException("Invalid credentials")
+
+        if (!hashEncoder.matches(password, user.hashedPassword))
+            throw BadCredentialsException("Invalid credentials")
+
+        val newAccessToken = jwtService.generateAccessToken(user.id.toHexString())
+        val newRefreshToken = jwtService.generateRefreshToken(user.id.toHexString())
+
+        storeRefreshToken(
+            userId = user.id,
+            rawRefreshToken = newRefreshToken
+        )
+
+        return TokenPair(
+            accessToken = newAccessToken,
+            refreshToken = newRefreshToken
+        )
+    }
+
+    fun refresh(refreshToken: String): TokenPair {
+        if (!jwtService.validateRefreshToken(refreshToken))
+            throw IllegalArgumentException("Invalid refresh token.")
+
+        val userId = jwtService.getUserIdFromToken(refreshToken)
+        val user = userRepository.findById(ObjectId(userId)).orElseThrow {
+            IllegalArgumentException("Invalid refresh token.")
+        }
+
+        val hashed = hashToken(refreshToken)
+        refreshTokenRepository.findByUserIdAndHashedToken(
+            userId = user.id,
+            hashedToken = hashed
+        ) ?: throw IllegalArgumentException("Refresh token not recognized")
+
+        refreshTokenRepository.deleteByUserIdAndHashedToken(
+            userId = user.id,
+            hashedToken = hashed
+        )
+
+        val newAccessToken = jwtService.generateAccessToken(userId)
+        val newRefreshToken = jwtService.generateRefreshToken(userId)
+
+        storeRefreshToken(
+            userId = user.id,
+            rawRefreshToken = newRefreshToken
+        )
+
+        return TokenPair(
+            accessToken = newAccessToken,
+            refreshToken = newRefreshToken
+        )
+
+    }
+
+    private fun storeRefreshToken(userId: ObjectId, rawRefreshToken: String) {
+        val hashed = hashToken(rawRefreshToken)
+        val expiryMs = jwtService.refreshTokenValidityMs
+        val expiresAt = Instant.now().plusMillis(expiryMs)
+
+        refreshTokenRepository.save(
+            RefreshToken(
+                userId = userId,
+                expiresAt = expiresAt,
+                hashedToken = hashed
+            )
+        )
+
+    }
+
+    private fun hashToken(token: String): String {
+        val digest = MessageDigest.getInstance("SHA-256")
+        val hashBytes = digest.digest(token.encodeToByteArray())
+        return Base64.getEncoder().encodeToString(hashBytes)
+    }
+
+}
